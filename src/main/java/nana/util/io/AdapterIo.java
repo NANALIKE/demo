4ffb627312165ed6792750839721f20379708715
nanav <selina555@qq.com>
2020-01-12 00:33:28 +0800

    提交代码

---
+++ src/main/java/nana/util/io/AdapterIo.java
@@ -0,0 +1,10 @@
+package nana.util.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class AdapterIo {
+    public static void main(String[] args) {
+    }
+}
